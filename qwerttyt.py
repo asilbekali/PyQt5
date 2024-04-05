fb687faca1466932108aaffd024faddb9fc66e5f
asilbekali <aliabdugafforov14@gmail.com>
2024-04-05 17:36:26 +0500

    4-oy imtixon savolidagi misol

--- qwerttyt.py
+++ qwerttyt.py
@@ -1,40 +1,29 @@
-from PyQt5.QtWidgets import QApplication, QLabel, QWidget, QLineEdit, QPushButton
-import sys
+class Time:
+    def __init__(self, hour, minute, second):
+        self.hour = hour
+        self.minute = minute
+        self.second = second
 
-def search():
-    number = txt.text()
-    try:
-        number = int(number)
-        numbers = {
-            1: 'bir', 2: 'ikki', 3: 'uch', 4: "to'rt", 5: 'besh', 6: 'olti', 7: 'yetti',
-            8: 'sakiz', 9: "to'qiz", 10: "o'n", 11: "o'n bir", 12: "o'n ikki", 13: "o'n uch",
-            14: "o'n to'rt", 15: "o'nbesh", 16: "o'n olti", 17: "o'n yetti", 18: "o'n sakkiz",
-            19: "o'n to'qiz", 20: "yigirma", 21: 'yigirma bir', 22: 'yigirma ikki',
-            23: 'yigirma uch', 24: "yigirma to'rt", 25: 'yigirma besh'
-        }
-        if 1 <= number <= 25:
-            label = QLabel(window)
-            label.setText(numbers[number])
-            label.move(300, 130)
-            label.show()
-    except ValueError:
-        pass
+    def output(self):
+        print(self.hour*60+self.minute+self.second/60, "minut bor\n\n")
 
-app = QApplication(sys.argv)
-window = QWidget()
-window.setGeometry(1000, 100, 700, 700)
-txt = QLineEdit(window)
-btn = QPushButton("Click", window)
-btn.setStyleSheet("QPushButton::hover"
-                  "{"
-                  "background-color : #00f365;"
-                  "}")
-btn.move(320, 60)
-txt.move(300, 100)
-soz = QLabel(window)
-soz.setText("25 orlig'ida son kiriting")
-soz.move(300, 150)
-soz.show()
-btn.clicked.connect(search)
-window.show()
-sys.exit(app.exec_())
+    def minute_count(self):
+        return self.hour * 60 + self.minute
+
+    def add_10_minutes(self):
+        total_minutes = self.minute_count() + 10
+        self.hour = total_minutes // 60
+        self.minute = total_minutes % 60
+
+objects = []
+for i in range(5):
+    hour, minute, second = map(int, input(f"Obyekt {i+1}: Soat:Daqiqa:Sekund : orqalik kiriting ").split(":"))
+    time_obj = Time(hour, minute, second)
+    objects.append(time_obj)
+
+for obj in objects:
+    if obj.hour == 23 and obj.minute > 49:
+        obj.hour = (obj.hour + 1) % 24
+    obj.output()
+    obj.add_10_minutes()
+    print("Obyekt", objects.index(obj)+1, "uchun 10 daqiqa qo'shilgandan kigin:", obj.hour, "soat", obj.minute, "daqiqa")
